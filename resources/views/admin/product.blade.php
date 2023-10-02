9b6dc4559f0a6fcf19af726ce98ff68afd8d3882
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-10-02 14:20:45 +0700

    templating product

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Product Dashboard') }}</div>
+                <div class="card-header" style="background-color: #274E13;">{{ __('Product Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
