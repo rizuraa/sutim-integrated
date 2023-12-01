3cceb171340e45d94b05306f14031f69a32fa8c8
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-01 21:18:55 +0700

    add request stuff

--- resources/views/kitchen/requestform.blade.php
+++ resources/views/kitchen/requestform.blade.php
@@ -21,11 +21,14 @@
                                 <label for="platform" class form-label">Keperluan</label>
                                 <input type="text" class="form-control" id="keperluan" name="keperluan" required>
                             </div>
-                
                             <div class="mb-3">
                                 <label for="delivery" class="form-label">tanggal</label>
                                 <input type="date" class="form-control" id="tanggal" name="tanggal" required>
                             </div>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button type="button" class="btn btn-primary" id="addFormItem">Tambah Barang</button>
+                            <button class="btn btn-success" type="submit">Submit All</button>
                         </div>
                   </div>
                 </div>
