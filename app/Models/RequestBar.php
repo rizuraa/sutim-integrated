23785441205d5fc3e0aac283a982f534761351f0
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-05 20:38:08 +0700

    model requestbar

--- app/Models/RequestBar.php
+++ app/Models/RequestBar.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\requestBarList;
 
 class RequestBar extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nama',
+        'keperluan',
+        'tgl_req',
+        'status'
+    ];
+
+    public function requestBarList(){
+        return $this->hasMany(requestBarList::class, 'id_request_bar');
+    }
 }
