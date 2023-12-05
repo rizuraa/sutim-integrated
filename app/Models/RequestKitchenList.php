4be9f6c76bd9a021b12196e53cac3832e31a5d95
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-05 20:34:31 +0700

    create model requestkitchenlist

---
+++ app/Models/RequestKitchenList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RequestKitchenList extends Model
+{
+    use HasFactory;
+}
