2db6a64afa3fad5d37e6a2b85503edefbd522cbb
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-12 20:06:55 +0700

    data table bar

--- resources/views/bar/layout.blade.php
+++ resources/views/bar/layout.blade.php
@@ -71,6 +71,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
 <script>
+    $(document).ready( function () {
+        $('#dataTable').DataTable();
+    });
+</script>
+<script>
     document.addEventListener('DOMContentLoaded', function () {
     var currentIndex = 0;
     var container = document.getElementById('itemListContainer');
