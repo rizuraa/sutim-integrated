36c28211c89f91bc663d98bbbf25068415308897
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-12 20:47:41 +0700

    bar detail done

--- resources/views/bar/detailRequest.blade.php
+++ resources/views/bar/detailRequest.blade.php
@@ -1,5 +1,43 @@
 @extends('bar.layout')
 
 @section('content')
-<h1>detail</h1>
+<div class="container">
+    <div class="row justify-content-center">                
+        <div class="col-md-12">                
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-black">Request Bar - {{$requestBar->nama}} </h6>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>Request Bar : {{$requestBar->tgl_req}}</p>
+                            <p>Nama Karyawan : {{$requestBar->nama}}</p>
+                            <p>Keperluan : {{$requestBar->keperluan}}</p>
+                            <p>Status : {{$requestBar->status}}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Nomor</th>
+                                    <th>nama</th>
+                                    <th>unit</th>
+                                    <th>qty</th>                                    
+                                </th>
+                                @foreach ($requestBar->RequestBarList as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $item->nama }}</td>
+                                    <td>{{ $item->unit }}</td>
+                                    <td>{{ $item->qty }}</td>                                    
+                                </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
